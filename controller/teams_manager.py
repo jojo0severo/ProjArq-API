bba4308bb7338d8236c30bf6240fa7b6dd4e1bc6
jojo0severo <33180595+jojo0severo@users.noreply.github.com>
2019-11-22 21:58:21 -0300

    Update teams_manager.py

--- controller/teams_manager.py
+++ controller/teams_manager.py
@@ -80,7 +80,7 @@ class TeamsManager:
 
         else:
             team = self.teams_db.get_team(team_name)
-            print("Got team")
+            print("Got team", team.json() if team is not None else team)
             if team is not None and team.admin_email == admin_email and len(team.members) == 1:
                 print("Team is able to be deleted")
                 return self.teams_db.remove_team(team_name)
